d9ed8868ebd6fd2a0dcd82eaee6cc88621e2981e
Camilo Carrillo <ccarrillom@gmail.com>
2016-09-25 20:56:11 +0200

    compiling version in 8_0_1

---
+++ DQM/RPCMonitorModule/test/seg2D2.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2015_cff')
+
+process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
+process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
+
+process.source = cms.Source("EmptySource")
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
+
+process.MessageLogger = cms.Service("MessageLogger")
+
+process.demo = cms.EDAnalyzer("RPCMonitorEfficiency",
+    fileName = cms.untracked.string('/tmp/carrillo/test.root'),
+    fileOut = cms.untracked.string('/tmp/carrillo/sectest.root'),
+
+    BlackListFile = cms.untracked.string('/afs/cern.ch/user/c/carrillo/RPC/blacklist/blacklist_23may.dat'),
+
+    debug = cms.untracked.bool(True),
+
+    makehtml = cms.untracked.bool(True),
+    prodimages = cms.untracked.bool(True),
+
+    statistics = cms.untracked.bool(True),
+
+    threshold = cms.untracked.double(50.0),
+
+    barrel = cms.untracked.bool(True),
+    endcap = cms.untracked.bool(True)
+)
+
+process.p = cms.Path(process.demo)
