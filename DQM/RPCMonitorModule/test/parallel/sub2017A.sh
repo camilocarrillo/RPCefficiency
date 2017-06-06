215ad84e418e0796ecc7cebf46f8a7b4a2cc1612
Camilo Carrillo <ccarrillom@gmail.com>
2017-06-06 16:39:49 +0200

    adding error time cut

---
+++ DQM/RPCMonitorModule/test/parallel/sub2017A.sh
@@ -0,0 +1,7 @@
+. submit_run.sh 2017A 295 340 v1
+. submit_run.sh 2017A 295 376 v1
+. submit_run.sh 2017A 295 381 v1
+. submit_run.sh 2017A 295 390 v1
+. submit_run.sh 2017A 295 463 v1
+. submit_run.sh 2017A 295 613 v1
+. submit_run.sh 2017A 295 655 v1
